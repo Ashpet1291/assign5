19dd3fbad276c2e2a0f7c4a2666c75d3225ea7b5
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2021-03-02 02:25:24 -0800

    Add files via upload

--- enc_server.c
+++ enc_server.c
@@ -192,7 +192,7 @@ int main(int argc, char *argv[]){
     	if (charsRead < 0){
       		error("ERROR reading from socket");
     	}  
-		 put buffer into plaintext to use later
+		// put buffer into plaintext to use later
 		strcat(plaintext, tempBuffer);
 		
 
