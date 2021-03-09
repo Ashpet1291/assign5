923e01d86d6d93946281bee09c27f6424e3f4a83
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2021-03-09 02:01:10 -0800

    Add files via upload

--- dec_client.c
+++ dec_client.c
@@ -144,11 +144,11 @@ int main(int argc, char *argv[]) {
   portNumber = atoi(argv[3]);
   
   // if decoding port tris to connect to port 1
-  printf("this is arg 0 %s\n", argv[0]);
-  printf("this is arg 1 %s\n", argv[1]);
+  printf("this is dec client arg 0 %s\n", argv[0]);
+  printf("this is dec arg 1 %s\n", argv[1]);
   printf("this is arg 2 %s\n", argv[2]);
   printf("this is arg 3 %s\n", argv[3]);
-  printf("this is arg 4 %s\n", argv[4]);
+//  printf("this is arg 4 %s\n", argv[4]);
 
 
   
