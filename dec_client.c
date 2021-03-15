db28b6fbd465db22703e901e4615296ae341b173
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2021-03-15 01:32:35 -0700

    Add files via upload

--- dec_client.c
+++ dec_client.c
@@ -335,7 +335,7 @@ int main(int argc, char *argv[]) {
 
 	//	int size = strlen(plaintext)-1;
 		cipherText[plainSize-1] = '\0';
-        printf("%s\n", ciphertext);
+        printf("%s\n", cipherText);
 //    // receives encrypted text from server
 //    // Clear out the buffer again for reuse
 //    memset(buffer, '\0', sizeof(buffer));
